56f161f32317029536f0191bba689a903e4890ab
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-21 16:39:54 +0700

    tích hợp keycloak vào angular

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Cấu hình Keycloak
+const keycloakConfig = {
+  url: 'http://localhost:8181',
+  realm: 'exam',
+  clientId: 'exam_angular_app',
+};
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: keycloakConfig,
+      initOptions: {
+        onLoad: 'login-required', // 'login-required' hoặc 'check-sso'
+        checkLoginIframe: false,
+      },
+    });
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
